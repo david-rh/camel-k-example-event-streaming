e204ae5c547a28db041d958c570bf16a944ead4e
David Anderson <david.anderson@redhat.com>
2022-11-10 12:32:55 -0600

    tmp fix url

--- front-end/src/main/resources/site/js/index.js
+++ front-end/src/main/resources/site/js/index.js
@@ -42,7 +42,7 @@ function renderTimelineInfoIncident(obj) {
 $(document).ready(function () {
     var path = $('[timeline]').attr('data-api')
 
-    var url = "timeline-bridge-camel-k-event-streaming.apps.ocp1.casaanderson.home" + path;
+    var url = "http://timeline-bridge-camel-k-event-streaming.apps.ocp1.casaanderson.home" + path;
 
     axios.get(url).then(function (response) {
         // console.log("Received " + response.data);
